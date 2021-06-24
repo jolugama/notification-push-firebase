cbc1d3e63f3302b8b161290e97df32d2b63fca88
Jose Luis G <garciamartinezjoseluis@gmail.com>
2021-06-24 13:52:24 +0200

    refactor. notification working.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
- let swReg;
+//  let swReg;
  let app = (() => {
     //  const url = window.location.href;
      let messageDisplayed = false;
@@ -232,3 +232,65 @@
 
 
  // notifyMe("holi","que tal?");
+
+
+
+
+
+
+
+
+
+
+
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyDzCa49qFHdSHVPCAGjSkbg0otzfdYYdNk",
+    authDomain: "mypwa-8c271.firebaseapp.com",
+    projectId: "mypwa-8c271",
+    storageBucket: "mypwa-8c271.appspot.com",
+    messagingSenderId: "475764615194",
+    appId: "1:475764615194:web:2db636ff8a5588b2f9c9a3",
+    measurementId: "G-3BQG8M4FLW"
+};
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+function IntitalizeFireBaseMessaging() {
+    messaging
+        .requestPermission()
+        .then(function () {
+            console.log("Notification Permission");
+            return messaging.getToken();
+        })
+        .then(function (token) {
+            console.log("Token : " + token);
+            document.getElementById("token").innerHTML = token;
+
+
+        })
+        .catch(function (reason) {
+            console.log(reason);
+        });
+}
+
+messaging.onMessage(function (payload) {
+    console.log(payload);
+    const notificationOption = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+    if (Notification.permission === "granted") {
+        var notification = new Notification(payload.notification.title, notificationOption);
+
+        notification.onclick = function (ev) {
+            ev.preventDefault();
+            window.open(payload.notification.click_action, '_blank');
+            notification.close();
+        }
+    }
+
+});
+
+

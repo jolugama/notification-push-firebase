945ec0246dd0c41c353b54a1366c30681e18ea95
Jose Luis G <garciamartinezjoseluis@gmail.com>
2021-06-28 14:42:24 +0200

    problem firebase production

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,17 +2,6 @@ importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-messaging.js');
 importScripts('js/config-firebase.js');
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyDzCa49qFHdSHVPCAGjSkbg0otzfdYYdNk",
-//     authDomain: "mypwa-8c271.firebaseapp.com",
-//     projectId: "mypwa-8c271",
-//     storageBucket: "mypwa-8c271.appspot.com",
-//     messagingSenderId: "475764615194",
-//     appId: "1:475764615194:web:2db636ff8a5588b2f9c9a3",
-//     measurementId: "G-3BQG8M4FLW"
-// };
-// firebase.initializeApp(firebaseConfig);
-// const messaging=firebase.messaging();
 
 // onBackgroundMessage
 // la vibración no consigo que se active en móvil

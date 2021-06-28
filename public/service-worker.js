82dd1c72fbfa4fb52b7ed6a69211d64844f12c89
Jose Luis G <garciamartinezjoseluis@gmail.com>
2021-06-28 13:39:39 +0200

    manifest error

--- public/service-worker.js
+++ public/service-worker.js
@@ -106,11 +106,11 @@ this.workbox.googleAnalytics.initialize();
 // this.workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 this.workbox.precaching.precacheAndRoute([{
-        url: 'notification-push-firebase/public/offline.html',
+        url: 'offline.html',
         revision: null
     },
     {
-        url: 'notification-push-firebase/public/images2/no-image.png',
+        url: 'images2/no-image.png',
         revision: null
     },
     // ... other entries ...
@@ -126,9 +126,9 @@ this.workbox.routing.setCatchHandler(async ({
     console.log('event.request.destination', event.request.destination);
     // Return the precached offline page if a document is being requested
     if (event.request.destination === 'document') {
-        return this.workbox.precaching.matchPrecache('/offline.html');
+        return this.workbox.precaching.matchPrecache('offline.html');
     } else if (event.request.destination === 'image') {
-        return this.workbox.precaching.matchPrecache('/images2/no-image.png');
+        return this.workbox.precaching.matchPrecache('images2/no-image.png');
     }
     return Response.error();
 });
